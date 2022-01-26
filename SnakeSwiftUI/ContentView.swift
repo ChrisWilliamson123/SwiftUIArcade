2324b5fae43d989ebca5c5f58683770fe5e13ce7
Chris Williamson <cjwilliamson109@gmail.com>
2022-01-26 19:37:37 +0000

    Moved some code around.

--- SnakeSwiftUI/ContentView.swift
+++ SnakeSwiftUI/ContentView.swift
@@ -30,15 +30,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-extension Image {
-    func centerCropped() -> some View {
-        GeometryReader { geo in
-            self
-            .resizable()
-            .scaledToFill()
-            .frame(width: geo.size.width, height: geo.size.height)
-            .clipped()
-        }
-    }
-}
