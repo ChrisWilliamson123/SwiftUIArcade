442561acca4657cb83586ae3af29e2e40f2413e1
Chris Williamson <cjwilliamson109@gmail.com>
2022-01-31 09:14:35 +0000

    Moved some stuff.

--- SnakeSwiftUI/Views/GamePausedView.swift
+++ SnakeSwiftUI/Views/GamePausedView.swift
@@ -8,20 +8,6 @@
 import SwiftUI
 import AVFoundation
 
-
-var audioPlayer: AVAudioPlayer?
-
-func playSound(sound: String, type: String) {
-    if let path = Bundle.main.path(forResource: sound, ofType: type) {
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
-            audioPlayer?.play()
-        } catch {
-            print("ERROR")
-        }
-    }
-}
-
 struct GamePausedView: View {
     @EnvironmentObject var settings: GameSettings
     @Binding var isPaused: Bool
